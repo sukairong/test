c35344fadee068e438055a0e9c5a722dc3a80352
sukr <kairong.su@accenture.com>
2017-10-24 10:55:28 +0800

    master modify during branch1

--- src/main/java/com/test/aa/App.java
+++ src/main/java/com/test/aa/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World for git modify!" );
+        System.out.println( "Hello 2 World for git modify! modify during branch exsit" );
     }
 }
