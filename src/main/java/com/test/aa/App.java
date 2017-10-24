7e9e9e54e8e0a3c46c102c1f84c1b80f365495c5
sukr <kairong.su@accenture.com>
2017-10-24 10:47:56 +0800

    branch1 modify

--- src/main/java/com/test/aa/App.java
+++ src/main/java/com/test/aa/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World for git modify!" );
-        System.out.println( "swich to branch1 " );
+        System.out.println( "swich to branch1 modify" );
     }
 }
