761f0d940471b58260dc6d504fb516f214192b26
sukr <kairong.su@accenture.com>
2017-10-24 10:42:23 +0800

    branch1 create2

--- src/main/java/com/test/aa/App.java
+++ src/main/java/com/test/aa/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World for git modify!" );
+        System.out.println( "swich to branch1 " );
     }
 }
